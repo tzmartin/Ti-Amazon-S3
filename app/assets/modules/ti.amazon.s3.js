4a4460f3b16982af1077d5e34e7d2f1523578a90
tdtsh <systemsfailing@gmail.com>
2014-10-19 20:15:48 +0900

    #2 now Android Supported JSDate -> Moment.js settable uploadDir ignore all build/*

--- app/assets/modules/ti.amazon.s3.js
+++ app/assets/modules/ti.amazon.s3.js
@@ -282,335 +282,6 @@ var UTF8 = function() {
   };
 };
 
-/*
- *
- * Date library
- *
-*/
-
-var JSDate = function() {
-  return {
-    MONTH_NAMES: new Array(
-  		'January',
-  		'February',
-  		'March',
-  		'April',
-  		'May',
-  		'June',
-  		'July',
-  		'August',
-  		'September',
-  		'October',
-  		'November',
-  		'December',
-  		'Jan','Feb',
-  		'Mar',
-  		'Apr',
-  		'May',
-  		'Jun',
-  		'Jul',
-  		'Aug',
-  		'Sep',
-  		'Oct',
-  		'Nov',
-  		'Dec'
-  	),
-
-  	DAY_NAMES: new Array(
-  		'Sunday',
-  		'Monday',
-  		'Tuesday',
-  		'Wednesday',
-  		'Thursday',
-  		'Friday',
-  		'Saturday',
-  		'Sun',
-  		'Mon',
-  		'Tue',
-  		'Wed',
-  		'Thu',
-  		'Fri',
-  		'Sat'
-  	),
-
-  	LZ: function(x) {return(x<0||x>9?"":"0")+x;},
-
-  	/* 
-  	 * isDate ( date_string, format_string )
-  	 * Returns true if date string matches format of format string and
-  	 * is a valid date. Else returns false.
-  	 * It is recommended that you trim whitespace around the value before
-  	 * passing it to this function, as whitespace is NOT ignored!
-  	*/
-  	isDate: function(val,format) {
-  		var date=this.getDateFromFormat(val,format);
-  		if (date==0) { return false; }
-  		return true;
-  	},
-
-  	/* 
-  	 * compareDates(date1,date1format,date2,date2format)
-  	 *   Compare two date strings to see which is greater.
-  	 *   Returns:
-  	 *   1 if date1 is greater than date2
-  	 *   0 if date2 is greater than date1 of if they are the same
-  	 *  -1 if either of the dates is in an invalid format
-  	*/
-  	compareDates: function(date1,dateformat1,date2,dateformat2) {
-  		var d1=this.getDateFromFormat(date1,dateformat1);
-  		var d2=this.getDateFromFormat(date2,dateformat2);
-  		if (d1==0 || d2==0) {
-  			return -1;
-  			}
-  		else if (d1 > d2) {
-  			return 1;
-  			}
-  		return 0;
-  	},
-
-  	/* 
-  	 * formatDate (date_object, format)
-  	 * Returns a date in the output format specified.
-  	 * The format string uses the same abbreviations as in getDateFromFormat()
-  	*/
-  	formatDate: function(date,format) {
-  		format=format+"";
-  		var result="";
-  		var i_format=0;
-  		var c="";
-  		var token="";
-  		var y=date.getYear()+"";
-  		var M=date.getMonth()+1;
-  		var d=date.getDate();
-  		var E=date.getDay();
-  		var H=date.getHours();
-  		var m=date.getMinutes();
-  		var s=date.getSeconds();
-  		var yyyy,yy,MMM,MM,dd,hh,h,mm,ss,ampm,HH,H,KK,K,kk,k;
-  		// Convert real date parts into formatted versions
-  		var value=new Object();
-  		if (y.length < 4) {y=""+(y-0+1900);}
-  		value["y"]=""+y;
-  		value["yyyy"]=y;
-  		value["yy"]=y.substring(2,4);
-  		value["M"]=M;
-  		value["MM"]=this.LZ(M);
-  		value["MMM"]=this.MONTH_NAMES[M-1];
-  		value["NNN"]=this.MONTH_NAMES[M+11];
-  		value["d"]=d;
-  		value["dd"]=this.LZ(d);
-  		value["E"]=this.DAY_NAMES[E+7];
-  		value["EE"]=this.DAY_NAMES[E];
-  		value["H"]=H;
-  		value["HH"]=this.LZ(H);
-  		if (H==0){value["h"]=12;}
-  		else if (H>12){value["h"]=H-12;}
-  		else {value["h"]=H;}
-  		value["hh"]=this.LZ(value["h"]);
-  		if (H>11){value["K"]=H-12;} else {value["K"]=H;}
-  		value["k"]=H+1;
-  		value["KK"]=this.LZ(value["K"]);
-  		value["kk"]=this.LZ(value["k"]);
-  		if (H > 11) { value["a"]="PM"; }
-  		else { value["a"]="AM"; }
-  		value["m"]=m;
-  		value["mm"]=this.LZ(m);
-  		value["s"]=s;
-  		value["ss"]=this.LZ(s);
-  		while (i_format < format.length) {
-  			c=format.charAt(i_format);
-  			token="";
-  			while ((format.charAt(i_format)==c) && (i_format < format.length)) {
-  				token += format.charAt(i_format++);
-  				}
-  			if (value[token] != null) { result=result + value[token]; }
-  			else { result=result + token; }
-  			}
-  		return result;
-  	},
-
-  	/* 
-  	 * Utility functions for parsing in getDateFromFormat()
-  	*/
-  	_isInteger: function(val) {
-  		var digits="1234567890";
-  		for (var i=0; i < val.length; i++) {
-  			if (digits.indexOf(val.charAt(i))==-1) { return false; }
-  			}
-  		return true;
-  	},
-
-  	_getInt: function(str,i,minlength,maxlength) {
-  		for (var x=maxlength; x>=minlength; x--) {
-  			var token=str.substring(i,i+x);
-  			if (token.length < minlength) { return null; }
-  			if (this._isInteger(token)) { return token; }
-  			}
-  		return null;
-  	},
-
-  	/* 
-  	 * getDateFromFormat( date_string , format_string )
-  	 *
-  	 * This function takes a date string and a format string. It matches
-  	 * If the date string matches the format string, it returns the 
-  	 * getTime() of the date. If it does not match, it returns 0.
-  	*/
-  	getDateFromFormat: function(val,format) {
-  		val=val+"";
-  		format=format+"";
-  		var i_val=0;
-  		var i_format=0;
-  		var c="";
-  		var token="";
-  		var token2="";
-  		var x,y;
-  		var now=new Date();
-  		var year=now.getYear();
-  		var month=now.getMonth()+1;
-  		var date=1;
-  		var hh=now.getHours();
-  		var mm=now.getMinutes();
-  		var ss=now.getSeconds();
-  		var ampm="";
-
-  		while (i_format < format.length) {
-  			// Get next token from format string
-  			c=format.charAt(i_format);
-  			token="";
-  			while ((format.charAt(i_format)==c) && (i_format < format.length)) {
-  				token += format.charAt(i_format++);
-  				}
-  			// Extract contents of value based on format token
-  			if (token=="yyyy" || token=="yy" || token=="y") {
-  				if (token=="yyyy") { x=4;y=4; }
-  				if (token=="yy")   { x=2;y=2; }
-  				if (token=="y")    { x=2;y=4; }
-  				year=_getInt(val,i_val,x,y);
-  				if (year==null) { return 0; }
-  				i_val += year.length;
-  				if (year.length==2) {
-  					if (year > 70) { year=1900+(year-0); }
-  					else { year=2000+(year-0); }
-  					}
-  				}
-  			else if (token=="MMM"||token=="NNN"){
-  				month=0;
-  				for (var i=0; i<this.MONTH_NAMES.length; i++) {
-  					var month_name=this.MONTH_NAMES[i];
-  					if (val.substring(i_val,i_val+month_name.length).toLowerCase()==month_name.toLowerCase()) {
-  						if (token=="MMM"||(token=="NNN"&&i>11)) {
-  							month=i+1;
-  							if (month>12) { month -= 12; }
-  							i_val += month_name.length;
-  							break;
-  							}
-  						}
-  					}
-  				if ((month < 1)||(month>12)){return 0;}
-  				}
-  			else if (token=="EE"||token=="E"){
-  				for (var i=0; i<this.DAY_NAMES.length; i++) {
-  					var day_name=this.DAY_NAMES[i];
-  					if (val.substring(i_val,i_val+day_name.length).toLowerCase()==day_name.toLowerCase()) {
-  						i_val += day_name.length;
-  						break;
-  						}
-  					}
-  				}
-  			else if (token=="MM"||token=="M") {
-  				month=this._getInt(val,i_val,token.length,2);
-  				if(month==null||(month<1)||(month>12)){return 0;}
-  				i_val+=month.length;}
-  			else if (token=="dd"||token=="d") {
-  				date=this._getInt(val,i_val,token.length,2);
-  				if(date==null||(date<1)||(date>31)){return 0;}
-  				i_val+=date.length;}
-  			else if (token=="hh"||token=="h") {
-  				hh=this._getInt(val,i_val,token.length,2);
-  				if(hh==null||(hh<1)||(hh>12)){return 0;}
-  				i_val+=hh.length;}
-  			else if (token=="HH"||token=="H") {
-  				hh=this._getInt(val,i_val,token.length,2);
-  				if(hh==null||(hh<0)||(hh>23)){return 0;}
-  				i_val+=hh.length;}
-  			else if (token=="KK"||token=="K") {
-  				hh=this._getInt(val,i_val,token.length,2);
-  				if(hh==null||(hh<0)||(hh>11)){return 0;}
-  				i_val+=hh.length;}
-  			else if (token=="kk"||token=="k") {
-  				hh=this._getInt(val,i_val,token.length,2);
-  				if(hh==null||(hh<1)||(hh>24)){return 0;}
-  				i_val+=hh.length;hh--;}
-  			else if (token=="mm"||token=="m") {
-  				mm=this._getInt(val,i_val,token.length,2);
-  				if(mm==null||(mm<0)||(mm>59)){return 0;}
-  				i_val+=mm.length;}
-  			else if (token=="ss"||token=="s") {
-  				ss=this._getInt(val,i_val,token.length,2);
-  				if(ss==null||(ss<0)||(ss>59)){return 0;}
-  				i_val+=ss.length;}
-  			else if (token=="a") {
-  				if (val.substring(i_val,i_val+2).toLowerCase()=="am") {ampm="AM";}
-  				else if (val.substring(i_val,i_val+2).toLowerCase()=="pm") {ampm="PM";}
-  				else {return 0;}
-  				i_val+=2;}
-  			else {
-  				if (val.substring(i_val,i_val+token.length)!=token) {return 0;}
-  				else {i_val+=token.length;}
-  				}
-  			}
-  		// If there are any trailing characters left in the value, it doesn't match
-  		if (i_val != val.length) { return 0; }
-  		// Is date valid for month?
-  		if (month==2) {
-  			// Check for leap year
-  			if ( ( (year%4==0)&&(year%100 != 0) ) || (year%400==0) ) { // leap year
-  				if (date > 29){ return 0; }
-  			}
-  			else { if (date > 28) { return 0; } }
-  			}
-  		if ((month==4)||(month==6)||(month==9)||(month==11)) {
-  			if (date > 30) { return 0; }
-  			}
-  		// Correct hours value
-  		if (hh<12 && ampm=="PM") { hh=hh-0+12; }
-  		else if (hh>11 && ampm=="AM") { hh-=12; }
-  		var newdate=new Date(year,month-1,date,hh,mm,ss);
-  		return newdate.getTime();
-  	},
-
-  	/*
-  	 * parseDate( date_string [, prefer_euro_format] )
-  	 *
-  	 * This function takes a date string and tries to match it to a
-  	 * number of possible date formats to get the value. It will try to
-  	 * match against the following international formats, in this order:
-  	 * y-M-d   MMM d, y   MMM d,y   y-MMM-d   d-MMM-y  MMM d
-  	 * M/d/y   M-d-y      M.d.y     MMM-d     M/d      M-d
-  	 * d/M/y   d-M-y      d.M.y     d-MMM     d/M      d-M
-  	 * A second argument may be passed to instruct the method to search
-  	 * for formats like d/M/y (european format) before M/d/y (American).
-  	 * Returns a Date object or null if no patterns match.
-  	*/
-  	parseDate: function(val) {
-  		var preferEuro=(arguments.length==2)?arguments[1]:false;
-  		generalFormats=new Array('y-M-d','MMM d, y','MMM d,y','y-MMM-d','d-MMM-y','MMM d');
-  		monthFirst=new Array('M/d/y','M-d-y','M.d.y','MMM-d','M/d','M-d');
-  		dateFirst =new Array('d/M/y','d-M-y','d.M.y','d-MMM','d/M','d-M');
-  		var checkList=new Array('generalFormats',preferEuro?'dateFirst':'monthFirst',preferEuro?'monthFirst':'dateFirst');
-  		var d=null;
-  		for (var i=0; i<checkList.length; i++) {
-  			var l=window[checkList[i]];
-  			for (var j=0; j<l.length; j++) {
-  				d=this.getDateFromFormat(val,l[j]);
-  				if (d!=0) { return new Date(d); }
-  				}
-  			}
-  		return null;
-  	}
-  };
-};
 
 /*
  *
@@ -628,7 +299,7 @@ var Module = function() {
   this.debug = false;
 	
   this.http = Ti.Network.createHTTPClient({
-    timeout:Module.timeout,
+    timeout: 99000,
     cache: false,
     onreadystatechange: function() {
       Ti.API.info(this.status);
@@ -646,7 +317,6 @@ var Module = function() {
    */
   this.SHA = new SHA();
   this.Utf8 = new UTF8();
-  this.Date = new JSDate();
   
   this.abort = function() {
     if (typeof this.http.abort == 'function') {
@@ -669,6 +339,7 @@ var Module = function() {
 Module.prototype.PUT = function(_args) {
 
 	this.fileName = _args.fileName;
+	var uploadDir = _args.uploadDir;
 		
 	if (_args.key) { Module.APIKey = _args.key; }
 	if (_args.secret) { Module.SecretKey = _args.secret; }
@@ -676,6 +347,7 @@ Module.prototype.PUT = function(_args) {
 	if (_args.fileName) { Module.fileName = _args.fileName; }
 	if (_args.timeout) { Module.timeout = _args.timeout; }
 	if (_args.debug) { Module.debug = _args.debug; }
+	if (_args.GSM) { Module.GSM = _args.GSM; }
 		
 	/*
   	 * Create HTTP Object
@@ -705,28 +377,31 @@ Module.prototype.PUT = function(_args) {
 		   return;
 		}
 		
-		this.fileURL = 'https://s3.amazonaws.com/'+Module.AWSBucketName+'/' + Module.fileName;
+		this.fileURL = 'https://'+Module.AWSBucketName+'.s3.amazonaws.com' + uploadDir + Module.fileName;
 		
 		this.log('File: '+this.fileURL);
 		
 		this.http.open('PUT', this.fileURL, true);
 		
-		var curDate = this.Date.formatDate(new Date(),'E, d MMM dd yyyy HH:mm:ss') + this.GSM;
-		var StringToSign = 'PUT\n\n\n'+fileContents.mimeType+'\n' + curDate + '\n/'+this.AWSBucketName+'/' + this.fileName;
-		
-		var AWSAccessKeyID = 'AWS ' + this.APIKey + ':';
-		var AWSSignature = this.SHA.b64_hmac_sha1(this.SecretKey, this.Utf8.encode(StringToSign));
+		var Moment = require('alloy/moment');
+		var curDate = Moment().format('ddd, D MMMM YYYY HH:mm:ss') + Module.GSM;
+		var StringToSign = ''+'PUT\n\n'+''+fileContents.mimeType+'\n' + '' + curDate + '\n/'+''+Module.AWSBucketName + uploadDir + Module.fileName;
+
+		var AWSAccessKeyID = 'AWS ' + Module.APIKey + ':';
+		var AWSSignature = this.SHA.b64_hmac_sha1(Module.SecretKey, this.Utf8.encode(StringToSign));
 		var AWSAuthHeader = AWSAccessKeyID.concat(AWSSignature);
 		
-		this.http.setRequestHeader('Authorization', Ti.Utils.base64encode(AWSAuthHeader).toString()); 
+		this.http.setRequestHeader('Authorization', AWSAuthHeader); 
+		if (OS_IOS) {
+			this.http.setRequestHeader('Content-Length', uploadFile.size);
+		}
 		this.http.setRequestHeader('Content-Type', fileContents.mimeType);
-		this.http.setRequestHeader('Content-Length', uploadFile.size);
-		this.http.setRequestHeader('Host', 's3.amazonaws.com');
+		this.http.setRequestHeader('Host', Module.AWSBucketName+'.s3.amazonaws.com');
 		this.http.setRequestHeader('Date', curDate);
 		this.http.setRequestHeader('Accept-Encoding', 'gzip');
 		this.http.setRequestHeader('Proxy-Connection','close');
 		this.http.setRequestHeader('User-Agent','Appcelerator Titanium/1.8.1.v20120126144634 (iPhone/5.0.1; iPhone OS; en_US;');
-		
+
 		this.http.send(fileContents);
 };
 
